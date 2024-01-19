6baf7939104db588d57079b97f68c7d202fc58d7
VitorSouza01 <souza.programing@gmail.com>
2024-01-18 23:17:30 -0300

    third commit

--- jogo_mega_sena.py
+++ jogo_mega_sena.py
@@ -5,24 +5,27 @@
 from random import sample
 
 
-# gera sequencia aleatoria, sem repetição, de números entre 1 e 60
-def gerar_jogo(dezenas_quantidade):
-    return sorted(list(sample(range(1, 61), dezenas_quantidade)))
+# Gerando sequencia aleatória, sem repetição, de números entre 1 e 60
+def gerar_jogo(dezenas):
+    return sorted(list(sample(range(1, 61), dezenas)))
 
 
-# gera uma lista, contendo as varias sequencias númericas, ilimitado a quantidade de jogos desejada
-def gerar_lista_jogos(jogos_quantidade, dezenas_quantidade):
+# Gerando uma lista, contendo as varias sequencias númericas, ilimitado a quantidade de jogos desejada
+def gerar_lista_jogos(jogos, dezenas):
     lista_jogos = list()
-    while len(lista_jogos) <= jogos_quantidade:
+
+    while len(lista_jogos) <= jogos:
         lista_jogos.append(
-            gerar_jogo(dezenas_quantidade)
+            gerar_jogo(dezenas)
         )
     return lista_jogos
 
 
 # Resultado
 print("[ GERADOR DE JOGOS DA MEGA SENA ]")
-
 quantidade_de_jogos = int(input("\n-Digite a quantidade de jogos que deseja fazer: "))
 
-print(gerar_lista_jogos(quantidade_de_jogos - 1, 6))
+lista_final = gerar_lista_jogos((quantidade_de_jogos - 1), 6)
+
+for n in range(quantidade_de_jogos):
+    print(lista_final[n])
